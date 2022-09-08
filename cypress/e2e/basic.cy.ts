c8ae5422cb55c8ed69a10e415a590fb190839e09
Oren Kredo <okredo@qarik.com>
2022-09-07 17:01:44 -0700

    new test

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -1,17 +1,16 @@
-describe('empty spec', () => {
+describe("empty spec", () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit("/");
+  });
 
-  it('displays the resources text', () => {
-    cy.get('h1')
-    .contains('Quickstart Template');
-  })
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
-})
+  it("displays the resources text", () => {
+    cy.get("Weather").contains("San Francisco, US");
+  });
+  // it("renders the image", () => {
+  //   cy.get("img")
+  //     .should("be.visible")
+  //     .and(($img) => {
+  //       expect($img[0].naturalWidth).to.be.greaterThan(0);
+  //     });
+  // });
+});
