653504b1e80854ce11711af5d1c120b9596c0ca0
Oren Kredo <okredo@qarik.com>
2022-09-07 17:03:54 -0700

    revert to front page to support shipped e2e cypress test

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -4,13 +4,13 @@ describe("empty spec", () => {
   });
 
   it("displays the resources text", () => {
-    cy.get("Weather").contains("San Francisco, US");
+    cy.get("h1").contains("Quickstart Template");
   });
-  // it("renders the image", () => {
-  //   cy.get("img")
-  //     .should("be.visible")
-  //     .and(($img) => {
-  //       expect($img[0].naturalWidth).to.be.greaterThan(0);
-  //     });
-  // });
+  it("renders the image", () => {
+    cy.get("img")
+      .should("be.visible")
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+  });
 });
